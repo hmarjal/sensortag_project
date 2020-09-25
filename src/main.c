827b5f0922551f535c7f68bdd46acd20d8c6fe07
Heikki Marjala <heikki.marjala@student.oulu.fi>
2020-09-25 11:42:52 +0300

    TEST 2

---
+++ src/main.c
@@ -0,0 +1,12 @@
+/* THIS IS TEST FILE FOR GITHUB CONNECTION
+ *  TRY 2
+ */
+
+#include <inttypes.h>
+#include <stdio.h>
+
+int main(void)
+{
+    printf("uint8_t max value is %hhu\n", UINT8_MAX);
+    return 0;
+}
